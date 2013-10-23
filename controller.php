840c0948d3ec2010486fc2f1411a31289acb75b9
Ryan Hewitt <ryan@mesuva.com.au>
2013-10-23 13:28:30 +1030

    Addition of DIR_REL to composer button path
    Addition of DIR_REL to composer button path, in case concrete5 is
    running in a sub-directory

--- controller.php
+++ controller.php
@@ -5,7 +5,7 @@ class ComposerEditButtonPackage extends Package {
 
 	protected $pkgHandle = 'composer_edit_button';
 	protected $appVersionRequired = '5.5.1';
-	protected $pkgVersion = '0.9';
+	protected $pkgVersion = '0.9.1';
 	
 	public function getPackageDescription() {
 		return t("A toolbar button to quickly edit pages via the composer");
@@ -23,7 +23,7 @@ class ComposerEditButtonPackage extends Package {
 	     
 	     $id = $req->getRequestCollectionID();
 	  
-	     $ihm->addPageHeaderMenuItem('composer', t('Edit In Composer'), 'left', array('class'=>'ccm-menu-icon ccm-icon-edit', 'href' => '/index.php/dashboard/composer/write/-/edit/' .  $id), 'composer_edit_button');         
+	     $ihm->addPageHeaderMenuItem('composer', t('Edit In Composer'), 'left', array('class'=>'ccm-menu-icon ccm-icon-edit', 'href' => DIR_REL . '/index.php/dashboard/composer/write/-/edit/' .  $id), 'composer_edit_button');         
 	 	}   
 	}
    
